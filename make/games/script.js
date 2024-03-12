00ef00df4b6539608a68a07298107bb0aad27bcc
slimesarecool <arinmanchannel@gmail.com>
2024-03-11 20:02:10 -0700

    Removed Redundant Code

--- make/games/script.js
+++
@@ -1,9 +0,0 @@
-var data = [
-	"https://itch.io/embed/1647979"
-]
-
-for (let i = 0; i < data.length; i++) {
-  console.log(data[i])
-
-	document.getElementById("games").innerHTML += " <iframe frameborder=\"0\" src=\"" + data[i] + "?dark=true\" width=\"552\" height=\"167\"></iframe>"
-}
